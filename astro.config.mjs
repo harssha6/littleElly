9de3c57c31a21de2e62e89730397773d6af7a146
sanatankc <sanatan.iit@gmail.com>
2024-03-01 14:42:17 +0530

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
+  site: "https://www.littleelly.com/",
+    integrations: [sitemap(), react(), tailwind()],
     output: "server",
     adapter: vercel(),
     prefetch: {
